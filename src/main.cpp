9aec58e7001271e2d7887b248bacefa6fa40de8f
uid04643 <rudolf.bauer@vitesco.com>
2025-08-04 17:42:00 +0200

    added button of LCD Keypad shield, mirror button state on LCD

--- src/main.cpp
+++ src/main.cpp
@@ -3,23 +3,52 @@
  * then off for one second, repeatedly.
  */
 #include "Arduino.h"
+#include <LcdMenu.h>
+#include <MenuScreen.h>
+#include <display/LiquidCrystalAdapter.h>
+#include <renderer/CharacterDisplayRenderer.h>
+
+
+// define the required LCD objects
+LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
+CharacterDisplayRenderer renderer(new LiquidCrystalAdapter(&lcd, 16, 2), 16, 2);
+LcdMenu menu(renderer);
+
 
 void setup()
 {
     pinMode(13, OUTPUT); // initialize LED digital pin as an output
+	pinMode(A0, INPUT);  // initialize A0 pin, which is connected to the buttons of Display Keypad, as input
+    
+    Serial.begin(115200); // we need the serial line for debugging; set the baudrate as parameter
+
+   renderer.begin();
+   lcd.clear();
+
+    // Some hello text
+    lcd.setCursor(0,0);
+	lcd.print("THI Experiment");
 }
 
 void loop()
 {
-    // turn the LED on (HIGH is the voltage level)
+    // turn the LED on (HIGH is the voltage level), put info on LCD
     digitalWrite(13, HIGH);
+	lcd.setCursor(0,1);
+	lcd.print("On ");
 
     // wait for 1s
     delay(1000);
 
-    // turn the LED off
-    digitalWrite(13, LOW);
+    // turn LED off, if switch is not pressed (i.e. if pin is high), put info on LCD
+    if (digitalRead(A0) == HIGH) 
+    {
+        digitalWrite(13, LOW);
+        lcd.setCursor(0,1);
+	    lcd.print("Off");
+    }
+    Serial.println("digital input: " + String(digitalRead(A0)));
 
-    // wait for 1s 
+    // wait for 1s, 
     delay(1000);
 }
