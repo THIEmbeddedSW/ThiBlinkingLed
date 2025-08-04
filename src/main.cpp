051309fd249c80bc7ebc70faf6e30b1428ddc893
RudolfBauerThalmassing <rudolf.bauer@thi.de>
2025-08-04 21:34:18 +0200

    added pin A0 check

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,9 @@
 void setup()
 {
     pinMode(13, OUTPUT); // initialize LED digital pin as an output
+    pinMode(A0, INPUT);  // initialize pin A0 of LCD Display Shield buttons as input
+
+    Serial.begin(115200); // we need the serial line for debugging
 }
 
 void loop()
@@ -17,8 +20,10 @@ void loop()
     // wait for 1s
     delay(1000);
 
-    // turn the LED off
-    digitalWrite(13, LOW);
+    // turn the LED off, if switch is not pressed (i.e. if pin is HIGH)
+    if (digitalRead(A0) == HIGH) digitalWrite(13, LOW);
+
+    Serial.println("digital input: " + String(digitalRead(A0)));
 
     // wait for 1s 
     delay(1000);
